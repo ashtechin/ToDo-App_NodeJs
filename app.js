eb05c48a2eefd5558ff51e5ec3134902df343f86
ashtechin <ashutoshbundela2@gmail.com>
2021-02-02 21:01:27 +0530

    c1

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app = express();
 app.set('view engine', 'ejs');
 
 //Static files handling
-app.use(express.static(__dirname + './public'));
+app.use(express.static('./public'));
 
 //run controllers
 todoController(app);
